59d1b0d36ec1f4c63bc81ccd9873ea439960af2e
Jork Elias <jork-elias@outlook.de>
2024-10-07 14:15:38 +0200

    updated download URL

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function LoadPage(){
 function clickedDownload(item){
     switch(item){
         case 0:
-            window.open("https://github.com/Joelbu537/ThingEncrypter/releases/download/release/Thing.exe");
+            window.open("https://github.com/Joelbu537/ThingEncrypter/releases/download/release/ThingEncrypter.1.1.exe");
             break;
     }
 }
