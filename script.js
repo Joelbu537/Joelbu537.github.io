6db7480a5d1628ffcee3c701952a250a08573bd4
Joelbu <89338010+Joelbu537@users.noreply.github.com>
2024-10-23 20:52:23 +0200

    added a new program to download, including a preview picture

--- script.js
+++ script.js
@@ -8,5 +8,7 @@ function clickedDownload(item){
         case 0:
             window.open("https://github.com/Joelbu537/ThingEncrypter/releases/download/release/ThingEncrypter.1.1.exe");
             break;
+        case 1:
+            window.open("https://github.com/Joelbu537/C-Tic-Tac-Toe/releases/download/release/tictactoe.exe");
     }
 }
